dcfec7a235788a7002afad01d7ca843f9c44c091
J4N8 <43821141+J4N8@users.noreply.github.com>
2023-07-20 12:29:40 +0200

    Initial commit

---
+++ src/main/java/me/j4n8/diplomskanaloga/DiplomskaNalogaApplication.java
@@ -0,0 +1,13 @@
+package me.j4n8.diplomskanaloga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomskaNalogaApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomskaNalogaApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/me/j4n8/diplomskanaloga/DiplomskaNalogaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.j4n8.diplomskanaloga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomskaNalogaApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
