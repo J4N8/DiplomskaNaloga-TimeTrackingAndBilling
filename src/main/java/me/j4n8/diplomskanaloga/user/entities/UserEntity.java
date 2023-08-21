db312c3c6a85a11860da7a3bc3ce9b50ca692588
J4N8 <43821141+J4N8@users.noreply.github.com>
2023-08-21 19:08:31 +0200

    Made username and email unique

--- src/main/java/me/j4n8/diplomskanaloga/user/entities/UserEntity.java
+++ src/main/java/me/j4n8/diplomskanaloga/user/entities/UserEntity.java
@@ -24,10 +24,10 @@ public class UserEntity implements UserDetails {
 	@Column(name = "id", nullable = false)
 	private Long id;
 	
-	@Column(name = "username", nullable = false)
+	@Column(name = "username", nullable = false, unique = true)
 	private String username;
 	
-	@Column(name = "email", nullable = false)
+	@Column(name = "email", nullable = false, unique = true)
 	private String email;
 	
 	@Column(name = "password", nullable = false)
